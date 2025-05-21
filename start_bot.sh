25c52f897a629b6506bd7325a7b4ab33630167d2
Shiziso <protezzy@ya.ru>
2025-05-22 00:40:54 +0300

    Delete start_bot.sh

--- start_bot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /root/telegram_bot
-source venv/bin/activate
-
-# Установка переменных окружения
-export TOKEN="ваш_токен_от_botfather"
-export ADMIN_USER_ID="ваш_id_пользователя"
-
-python3 psihotips_bot_updated_v6.py
